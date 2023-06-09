008bb4e0f98c475269a58e91094a8e89c7dc9b8a
VolodymyrCherniakov <115033891+VolodymyrCherniakov@users.noreply.github.com>
2023-06-09 12:28:41 +0200

    Add files via upload

---
+++ src/main/java/utils/FilmGenre.java
@@ -0,0 +1,16 @@
+package utils;
+
+public enum FilmGenre {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    ROMANCE,
+    SCIENCE_FICTION,
+    THRILLER,
+    ANIMATION,
+    DOCUMENTARY,
+    FANTASY,
+    ADVENTURE,
+    HISTORICAL
+}
